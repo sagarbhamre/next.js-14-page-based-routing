d3269fe4297dfe8249a03af4b8008e7e94250bb4
sagarbhmr@gmail.com <sagarbhr@gmail.com>
2024-01-09 12:40:46 +0530

    404 route and router.push method for navigation

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,9 +5,18 @@ function ClientProjectPage(){
     const router = useRouter();
     console.log(router.query);
 
+    function loadProjectHandler(){ 
+        // load data...
+        router.push({
+            pathname:'/clients/[id]/[clientprojectid]',
+            query: {id: 'sagar', clientprojectid: 'projecta' }
+        });
+    }
+
     return(
         <div>
             <h1>The Projects of a Given Client</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     )
 }
